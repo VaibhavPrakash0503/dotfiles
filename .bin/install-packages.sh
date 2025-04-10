ed95ce73fec30baceea49cf781d27e40808b5ca9
Vaibhav Prakash <vaibhavprakash549@gamil.com>
2025-04-10 14:28:14 +0530

    Added a new installation script file

---
+++ .bin/install-packages.sh
@@ -0,0 +1,90 @@
+#!/usr/bin/bash
+
+# This script installs my packages.
+
+
+# Define arrays 
+
+# terminal packages
+terminal_packages=(
+zsh
+ghostty
+tmux
+starship
+vim
+nvim
+bat
+fzf
+fastfetch
+yazi
+)
+
+# Softwares
+software=(
+vlc
+grub-customizer
+gnome-tweaks
+code
+)
+
+# Enable third-party repositories
+echo "Enabling third-party repositories..."
+
+# 1. Vs-code
+if ! sudo dnf repolist | grep -q "code"; then
+  echo "Adding VS Code repository..."
+  sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
+echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\nautorefresh=1\ntype=rpm-md\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" | sudo tee /etc/yum.repos.d/vscode.repo > /dev/null
+else
+  echo "VS Code repo already enabled"
+fi
+
+
+# 2. Ghostty
+if ! sudo dnf copr list | grep "pgdev/ghostty"; then
+  echo "Enabling Ghostty COPR..."
+  sudo dnf copr enable pgdev/ghostty -y
+else
+  echo "Ghostty COPR already eanbled"
+fi
+
+# 3. yazi
+if ! sudo dnf copr list | grep "lihaohong/yazi"; then
+  echo "Enabling yazi COPR..."
+  sudo dnf copr enable lihaohong/yazi -y
+else
+  echo "yazi COPR already enabled"
+fi
+
+# Install a package with error handling
+install_package() {
+  local package=$1
+  echo "Installing $package..."
+
+  if [ "$package" == "starship" ]; then
+    if ! command -v starship &> /dev/null; then
+      STARSHIP_INSTALL=yes curl -sS https://starship.rs/install.sh | sh
+    else
+      echo "Starship already installed"
+    fi
+  else
+    if ! sudo dnf install -y "$package"; then
+      echo "Error installing $package"
+    else
+      echo "$package installed successfully"
+    fi
+  fi
+  echo "------------------------------"
+}
+
+#Install terminal_packages
+echo "Installing terminal packages..."
+for pkg in "${terminal_packages[@]}"; do
+  install_package "$pkg"
+done
+
+# Install software
+echo "Installing GUI software..."
+for pkg in "${software[@]}"; do
+  install_package "$pkg"
+done
