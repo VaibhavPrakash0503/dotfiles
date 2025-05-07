92a89766a74cf6e353e51d84c61e77f2e04dc492
Vaibhav Prakash <vaibhavprakash549@gamil.com>
2025-05-07 23:49:37 +0530

    Added lines for handling flatpak apps

--- bin/.bin/update_fedora.sh
+++ bin/.bin/update_fedora.sh
@@ -3,16 +3,21 @@
 #This script updates system and software
 
 echo "Starting the update process..."
-echo "Updating system packages..."
 
+echo "Updating system packages..."
 sudo dnf update -y || {echo "System update failed!"; exit 1;}
+
+echo "Updating Flatpak packages..."
+flatpak update -y || { echo "Flatpak update failed!"; exit 1; }
 
 # Optionally, clean up old packages
 echo "Cleaning up old packages..."
-
 sudo dnf autoremove -y || { echo "Failed to clean up packages!"; exit 1; }
 
-echo "Cleaning cache and memory"
+echo "Cleaning DNF cache..."
 sudo dnf clean all || { echo "Failed to clean cache!"; exit 1; }
+
+echo "Cleaning unused Flatpak runtimes..."
+flatpak uninstall --unused -y || { echo "Failed to clean Flatpak runtimes!"; exit 1; }
 
 echo "Update process complete!"
