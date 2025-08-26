e97adcbce24787b5c7ce9d2141c2384ac970fe3b
Vaibhav Prakash <vaibhavprakash549@gmail.com>
2025-08-27 00:20:11 +0530

    Added keybinds to use Compiler.

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,8 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- Compiler plugin keymaps
+vim.keymap.set("n", "<F5>", "<cmd>CompilerOpen<cr>", { noremap = true, silent = true })
+vim.keymap.set("n", "<F6>", "<cmd>CompilerStop<cr><cmd>CompilerRedo<cr>", { noremap = true, silent = true })
+vim.keymap.set("n", "<F7>", "<cmd>CompilerToggleResults<cr>", { noremap = true, silent = true })
