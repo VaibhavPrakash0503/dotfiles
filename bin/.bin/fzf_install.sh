038946abcd01dc4a2be0462fd9085c81052921f7
Vaibhav Prakash <vaibhavprakash549@gmail.com>
2025-10-03 01:37:28 +0530

    Added support for flatpak apps

--- bin/.bin/fzf_install.sh
+++ bin/.bin/fzf_install.sh
@@ -1,11 +1,33 @@
 #!/usr/bin/env bash
 
-# DNF searcher in tmux floating window
 tmux display-popup -E -h 70% -w 60% -x C -y C "
-  dnf list --available --quiet | awk '{print \$1}' | sed 's/\\..*$//' | sort -u | fzf --print-query \
-      --bind='change:reload(if [ {q} ]; then dnf list --available --quiet | awk \"{print \\\$1}\" | sed \"s/\\..*$//\" | grep -i {q} | sort -u; else dnf list --available --quiet | awk \"{print \\\$1}\" | sed \"s/\\..*$//\" | sort -u; fi)' \
-      --bind='enter:execute(echo {} | xargs -r sudo dnf install -y)+abort' \
-      --preview='if [ {} ]; then dnf info {} 2>/dev/null; fi' \
+  (dnf list --available --quiet \
+    | awk '{print \$1}' \
+    | sed 's/\\..*\$//' \
+    | grep -v -E '^(lib|glibc|kernel|systemd|dbus|grub|dracut|font|NetworkManager)' \
+    | grep -v -E '(devel|debug|debuginfo|doc|lang|headers|common|data|libs?)\$' \
+    | sed 's/\$/\\t[rpm]/' \
+    | sed 's/\$/\\t[rpm]/' | head -200;
+
+   flatpak remote-ls flathub --app --columns=application \
+   | sed 's/\$/\\t[flatpak]/' | head -200) \
+   | sort | fzf --with-nth=1 --delimiter='\t' \
+      --bind='change:reload(
+        if [ {q} ]; then
+          (dnf list --available --quiet | awk \"{print \\\$1}\" | sed \"s/\\..*\$//\" | grep -i {q} | sed \"s/\$/\\t[rpm]/\"; \
+           flatpak remote-ls flathub --app --columns=application | grep -i {q} | sed \"s/\$/\\t[flatpak]/\") | sort;
+        else
+          (dnf list --available --quiet | awk \"{print \\\$1}\" | sed \"s/\\..*\$//\" | sed \"s/\$/\\t[rpm]/\" | head -200; \
+           flatpak remote-ls flathub --app --columns=application | sed \"s/\$/\\t[flatpak]/\" | head -200) | sort;
+        fi)' \
+      --bind='enter:execute(
+        if [[ {2} == \"[flatpak]\" ]]; then
+          flatpak install -y flathub {1};
+        else
+          sudo dnf install -y {1};
+        fi
+      )+abort' \
+      --preview='if [[ {2} == \"[flatpak]\" ]]; then flatpak remote-info flathub {1} 2>/dev/null; else dnf info {1} 2>/dev/null; fi' \
       --preview-window='down:50%' \
       --border='rounded' \
       --height='100%'
--- bin/.bin/fzf_remove.sh
+++ bin/.bin/fzf_remove.sh
@@ -1,11 +1,22 @@
 #!/usr/bin/env bash
 
-# DNF package remover - simplified (like your install script)
+# Combined package remover
 tmux display-popup -E -h 70% -w 60% -x C -y C "
-  rpm -qa --qf '%{NAME}\n' | grep -v -E '^(lib|font|glibc|kernel|systemd|dbus|NetworkManager|grub|dracut)' | grep -v -E '(devel|debug|doc|lang|headers)\$' | sort | fzf --print-query \
-      --bind='change:reload(if [ {q} ]; then rpm -qa --qf \"%{NAME}\\n\" | grep -i {q} | grep -v -E \"^(lib|font|glibc|kernel|systemd|dbus|NetworkManager|grub|dracut)\" | grep -v -E \"(devel|debug|doc|lang|headers)\\$\" | sort; else rpm -qa --qf \"%{NAME}\\n\" | grep -v -E \"^(lib|font|glibc|kernel|systemd|dbus|NetworkManager|grub|dracut)\" | grep -v -E \"(devel|debug|doc|lang|headers)\\$\" | sort ; fi)' \
-      --bind='enter:execute(echo {} | xargs -r sudo dnf remove -y)+abort' \
-      --preview='if [ {} ]; then dnf info {} 2>/dev/null; fi' \
+  (rpm -qa --qf '%{NAME}\t[rpm]\n' \
+    | grep -v -E '^(lib|font|glibc|kernel|systemd|dbus|NetworkManager|grub|dracut)' \
+    | grep -v -E '(devel|debug|doc|lang|headers)\$'; \
+    flatpak list --app --columns=application \
+    | sed 's/$/\t[flatpak]/') | sort | fzf --print-query --with-nth=1 --delimiter='\t' \
+      --bind='change:reload(if [ {q} ]; then (rpm -qa --qf \"%{NAME}\t[rpm]\n\" \
+      | grep -i {q} | grep -v -E \"^(lib|font|glibc|kernel|systemd|dbus|NetworkManager|grub|dracut)\" \
+      | grep -v -E \"(devel|debug|doc|lang|headers)\$\"; flatpak list --app --columns=application \
+      | grep -i {q} | sed \"s/$/\t[flatpak]/\") | sort; else (rpm -qa --qf \"%{NAME}\t[rpm]\n\" \
+      | grep -v -E \"^(lib|font|glibc|kernel|systemd|dbus|NetworkManager|grub|dracut)\" \
+      | grep -v -E \"(devel|debug|doc|lang|headers)\$\"; flatpak list --app --columns=application \
+      | sed \"s/$/\t[flatpak]/\") | sort; fi)' \
+      --bind='enter:execute(if [[ {2} == \"[flatpak]\" ]]; then flatpak uninstall -y --delete-data {1}; \
+    else sudo dnf remove -y {1}; fi)+abort' \
+      --preview='if [[ {2} == \"[flatpak]\" ]]; then flatpak info {1} 2>/dev/null; else dnf info {1} 2>/dev/null; fi' \
       --preview-window='down:50%' \
       --border='rounded' \
       --height='100%'
