f9674b4d5287fec9eace4e6b88ea901aebd5c730
Vaibhav Prakash <vaibhavprakash549@gmail.com>
2025-10-03 00:19:37 +0530

    Feature add to show all the available and installed packages in respective scripts

--- bin/.bin/fzf_install.sh
+++ bin/.bin/fzf_install.sh
@@ -1,10 +1,9 @@
 #!/usr/bin/env bash
 
 # DNF searcher in tmux floating window
-
 tmux display-popup -E -h 70% -w 60% -x C -y C "
-  printf 'firefox\ngit\nneovim\nhtop\ncode\nnodejs\npython3\nrust\n' | fzf --print-query \
-      --bind='change:reload(if [ {q} ]; then dnf search {q} 2>/dev/null | grep \"^[^=]\" | head -50 | awk \"{print \\\$1}\" | sed \"s/\\\\..*//\"; else printf \"firefox\\\\ngit\\\\nvim\\\\nhtop\\\\nchrome\\\\ncode\\\\ndocker\\\\nnodejs\\\\npython3\\\\nrust\\\\n\"; fi)' \
+  dnf list --available --quiet | awk '{print \$1}' | sed 's/\\..*$//' | sort -u | fzf --print-query \
+      --bind='change:reload(if [ {q} ]; then dnf list --available --quiet | awk \"{print \\\$1}\" | sed \"s/\\..*$//\" | grep -i {q} | sort -u; else dnf list --available --quiet | awk \"{print \\\$1}\" | sed \"s/\\..*$//\" | sort -u; fi)' \
       --bind='enter:execute(echo {} | xargs -r sudo dnf install -y)+abort' \
       --preview='if [ {} ]; then dnf info {} 2>/dev/null; fi' \
       --preview-window='down:50%' \
--- bin/.bin/fzf_remove.sh
+++ bin/.bin/fzf_remove.sh
@@ -1,10 +1,9 @@
 #!/usr/bin/env bash
 
 # DNF package remover - simplified (like your install script)
-
 tmux display-popup -E -h 70% -w 60% -x C -y C "
-  rpm -qa --qf '%{NAME}\n' | grep -v -E '^(lib|font|glibc|kernel|systemd|dbus|NetworkManager|grub|dracut)' | grep -v -E '(devel|debug|doc|lang|headers)\$' | sort | head -20 | fzf --print-query \
-      --bind='change:reload(if [ {q} ]; then rpm -qa --qf \"%{NAME}\\n\" | grep -i {q} | grep -v -E \"^(lib|font|glibc|kernel|systemd|dbus|NetworkManager|grub|dracut)\" | grep -v -E \"(devel|debug|doc|lang|headers)\\$\" | sort; else rpm -qa --qf \"%{NAME}\\n\" | grep -v -E \"^(lib|font|glibc|kernel|systemd|dbus|NetworkManager|grub|dracut)\" | grep -v -E \"(devel|debug|doc|lang|headers)\\$\" | sort | head -20; fi)' \
+  rpm -qa --qf '%{NAME}\n' | grep -v -E '^(lib|font|glibc|kernel|systemd|dbus|NetworkManager|grub|dracut)' | grep -v -E '(devel|debug|doc|lang|headers)\$' | sort | fzf --print-query \
+      --bind='change:reload(if [ {q} ]; then rpm -qa --qf \"%{NAME}\\n\" | grep -i {q} | grep -v -E \"^(lib|font|glibc|kernel|systemd|dbus|NetworkManager|grub|dracut)\" | grep -v -E \"(devel|debug|doc|lang|headers)\\$\" | sort; else rpm -qa --qf \"%{NAME}\\n\" | grep -v -E \"^(lib|font|glibc|kernel|systemd|dbus|NetworkManager|grub|dracut)\" | grep -v -E \"(devel|debug|doc|lang|headers)\\$\" | sort ; fi)' \
       --bind='enter:execute(echo {} | xargs -r sudo dnf remove -y)+abort' \
       --preview='if [ {} ]; then dnf info {} 2>/dev/null; fi' \
       --preview-window='down:50%' \
