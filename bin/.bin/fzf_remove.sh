31a6df16759702046a9015ed4fa8189f87785ae2
Vaibhav Prakash <vaibhavprakash549@gmail.com>
2025-10-02 23:54:03 +0530

    Minor changes

--- bin/.bin/fzf_remove.sh
+++ bin/.bin/fzf_remove.sh
@@ -4,8 +4,6 @@
 
 tmux display-popup -E -h 70% -w 60% -x C -y C "
   rpm -qa --qf '%{NAME}\n' | grep -v -E '^(lib|font|glibc|kernel|systemd|dbus|NetworkManager|grub|dracut)' | grep -v -E '(devel|debug|doc|lang|headers)\$' | sort | head -20 | fzf --print-query \
-      --prompt='Remove packages: ' \
-      --header='Type to search | Enter to remove' \
       --bind='change:reload(if [ {q} ]; then rpm -qa --qf \"%{NAME}\\n\" | grep -i {q} | grep -v -E \"^(lib|font|glibc|kernel|systemd|dbus|NetworkManager|grub|dracut)\" | grep -v -E \"(devel|debug|doc|lang|headers)\\$\" | sort; else rpm -qa --qf \"%{NAME}\\n\" | grep -v -E \"^(lib|font|glibc|kernel|systemd|dbus|NetworkManager|grub|dracut)\" | grep -v -E \"(devel|debug|doc|lang|headers)\\$\" | sort | head -20; fi)' \
       --bind='enter:execute(echo {} | xargs -r sudo dnf remove -y)+abort' \
       --preview='if [ {} ]; then dnf info {} 2>/dev/null; fi' \
