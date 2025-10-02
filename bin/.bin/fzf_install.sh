87b48e56744c8387c93da7d702935ac2a62d1853
Vaibhav Prakash <vaibhavprakash549@gmail.com>
2025-10-02 23:55:34 +0530

    Minor changes

--- bin/.bin/fzf_install.sh
+++ bin/.bin/fzf_install.sh
@@ -3,7 +3,7 @@
 # DNF searcher in tmux floating window
 
 tmux display-popup -E -h 70% -w 60% -x C -y C "
-  printf 'firefox\ngit\nneovim\nhtop\nchrome\ncode\nnodejs\npython3\nrust\n' | fzf --print-query \
+  printf 'firefox\ngit\nneovim\nhtop\ncode\nnodejs\npython3\nrust\n' | fzf --print-query \
       --bind='change:reload(if [ {q} ]; then dnf search {q} 2>/dev/null | grep \"^[^=]\" | head -50 | awk \"{print \\\$1}\" | sed \"s/\\\\..*//\"; else printf \"firefox\\\\ngit\\\\nvim\\\\nhtop\\\\nchrome\\\\ncode\\\\ndocker\\\\nnodejs\\\\npython3\\\\nrust\\\\n\"; fi)' \
       --bind='enter:execute(echo {} | xargs -r sudo dnf install -y)+abort' \
       --preview='if [ {} ]; then dnf info {} 2>/dev/null; fi' \
