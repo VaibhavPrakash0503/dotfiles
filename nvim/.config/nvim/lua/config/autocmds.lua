2a940ab5acc0ba3ed27fc9217936396a9d6b0edd
Vaibhav Prakash <vaibhavprakash549@gmail.com>
2025-10-17 20:22:34 +0530

    makes cursor back to | when leaving vim

--- nvim/.config/nvim/lua/config/autocmds.lua
+++ nvim/.config/nvim/lua/config/autocmds.lua
@@ -6,3 +6,11 @@
 --
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+
+-- Reset cursor to vertical bar on exit
+vim.api.nvim_create_autocmd("VimLeave", {
+  pattern = "*",
+  callback = function()
+    vim.cmd("set guicursor=a:ver25-blinkon1")
+  end,
+})
