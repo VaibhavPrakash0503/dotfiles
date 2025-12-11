834488a23145d57ccdefe69df0f6671559c17751
Vaibhav Prakash <vaibhavprakash549@gmail.com>
2025-12-11 21:23:49 +0530

    Added Live Preview plugin and some new keybinds

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -3,3 +3,6 @@
 -- Add any additional keymaps here
 -- Makes jj to escape to normal mode
 vim.api.nvim_set_keymap("i", "jj", "<Esc>", { noremap = false })
+vim.api.nvim_set_keymap("i", "jk", "<Esc>", { noremap = false })
+vim.api.nvim_set_keymap("n", "<leader>lp", ":LivePreview start<CR>", { desc = "Start Live Preview" })
+vim.api.nvim_set_keymap("n", "<leader>ls", ":LivePreview stop<CR>", { desc = "Stop Live Preview" })
